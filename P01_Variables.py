34a0f5e17860ae4d615d0e2090b6e2820667e7e5
VictorM8464 <143574007+VictorM8464@users.noreply.github.com>
2023-09-17 21:31:25 -0600

    Add files via upload

---
+++ P01_Variables.py
@@ -0,0 +1,16 @@
+#Para imprimir algo en pantalla se usa 'print'
+mensaje="Hola mundo :D"
+print(mensaje)
+
+#Las variables pueden contener cualquier tipo de dato
+mensaje=10
+print(mensaje)
+
+#Se pueden hacer operaciones adentro de print sin alterar el dato de la varible
+print(mensaje + 20)
+
+#Es la suma de dos variables
+Num1=15
+Num2=35
+Num3=Num1+Num2
+print(Num3)
---
+++ P02_String.py
@@ -0,0 +1,7 @@
+saludo1="Hola"
+saludo2=' Mundo'
+print(saludo1+saludo2)
+#Si quieres poner "" en tu mensaje, debes poner ''
+#Funciona en viceversa
+nota='"print()" se utiliza para imprimir valores en la consola.'
+print(nota)
