6ff3de2d3dd40e99113acac0a5e4467448e4a177
VictorM8464 <143574007+VictorM8464@users.noreply.github.com>
2023-09-17 22:58:26 -0600

    Add files via upload

---
+++ P20_Bucle_While.py
@@ -0,0 +1,21 @@
+#While es bucle que te perminte repetir un proceso hasta que se cumpla una condicion
+print('\tPrimer bucle\n')
+x = 0
+
+while x <= 15:
+	print(x)
+	x+=5
+
+print('\tSegundo bucle\n')
+x = -100
+
+while x <= 0:
+	print(x)
+	x-=-20
+	
+print('\tTercer bucle\n')
+x = 10
+
+while x >= 0:
+	print('El valor del bucle es: ', x)
+	x-=1
