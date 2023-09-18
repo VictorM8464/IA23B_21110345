1200bb03e0647d032365cc56bec6c89362ce12b0
VictorM8464 <143574007+VictorM8464@users.noreply.github.com>
2023-09-17 21:32:11 -0600

    Delete P4_Formato_String.py

--- P4_Formato_String.py
+++
@@ -1,9 +0,0 @@
-MensajeTitle="enrique barros fernandez"
-MensajeTitle=MensajeTitle.title()
-print(MensajeTitle)
-
-MensajeUpper="Esta Es Una Frase Para Ser Formateada.".upper()
-print(MensajeUpper)
-
-MensajeLower="Esta Es Una Frase Para Ser Formateada.".lower()
-print(MensajeLower)
