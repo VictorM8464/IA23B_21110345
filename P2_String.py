d94ace9dfe94dd5783d5b15db04bc93330d90d40
VictorM8464 <143574007+VictorM8464@users.noreply.github.com>
2023-09-17 21:31:55 -0600

    Delete P2_String.py

--- P2_String.py
+++
@@ -1,7 +0,0 @@
-saludo1="Hola"
-saludo2=' Mundo'
-print(saludo1+saludo2)
-#Si quieres poner "" en tu mensaje, debes poner ''
-#Funciona en viceversa
-nota='"print()" se utiliza para imprimir valores en la consola.'
-print(nota)
